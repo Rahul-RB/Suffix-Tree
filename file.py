b10f7e83799127fa65bd3df773caf0fe5f584e0a
Rahul-RB <rahulbharadwaj033@gmail.com>
2017-10-15 16:11:53 +0530

    Final Commit

--- file.py
+++
@@ -1,8 +0,0 @@
-def words_in_string(word_list, a_string):
-	return set(word_list).intersection(a_string.split())
-
-my_word_list = ['one', 'two', 'three']
-a_string = 'one tw'
-
-for word in words_in_string(my_word_list, a_string):
-	print(word)
